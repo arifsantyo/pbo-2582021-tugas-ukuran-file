f90f5b4b6893204a149011bf4e83d7385fddf265
Irajoasa Peranginangin <2281066@unai.edu>
2026-09-14 11:20:37 +0700

    selesai

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -11,5 +11,26 @@ public class UkuranFile {
         System.out.print("Nama file     : ");
         String namaFile = input.nextLine();
 
+        System.out.print("Ukuran (byte) : ");
+        ukuranByte = input.nextLong();
+
+        double ukuranKB = ukuranByte / SATU_KB;
+        double ukuranMB = ukuranKB / SATU_KB;
+        double ukuranGB = ukuranMB / SATU_KB;
+
+        int ukuranMBBulat = (int) ukuranMB;
+
+        double selisihPembulatan = ukuranMB - ukuranMBBulat;
+
+        System.out.println();
+        System.out.println("===== UKURAN FILE =====");
+        System.out.println(namaFile);
+        System.out.println("  " + ukuranByte + " byte");
+        System.out.println("  " + ukuranKB + " KB");
+        System.out.println("  " + ukuranMB + " MB");
+        System.out.println("  " + ukuranGB + " GB");
+        System.out.println();
+        System.out.println("Dibulatkan ke MB  : " + ukuranMBBulat);
+        System.out.println("Selisih pembulatan: " + selisihPembulatan);
     }
 }
